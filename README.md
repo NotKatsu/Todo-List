a897a22833ec3fde5e6e8cc182d9b55ecc0aa325
NotKatsu <katsudiscord@outlook.com>
2023-02-20 21:58:22 +0000

    Quick recode

--- README.md
+++
@@ -1,2 +0,0 @@
-# Todo-List
-A to-do list coded in typescript to try and help me learn the basics of the language.
--- todo-list/src/pages/index.tsx
+++ todo-list/src/pages/index.tsx
@@ -1,8 +1,27 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import styles from '@/styles/Home.module.css'
+import { Card, Grid, Text, Container } from "@nextui-org/react";
+
 
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Container display="flex" justify="center" alignItems="center" css={{ "minHeight": "100vh" }}>
+        <Card css={{ "mw":"700px", "shadow": "none", "bg": "#2A2B2B" }}>
+          <Grid.Container>
+            <Card.Body>
+              <Text h2 css={{ "marginLeft": "auto", "marginRight": "auto" }}>Todo List</Text>
+            </Card.Body>
+          </Grid.Container>
+          
+          <Grid.Container>
+            <Card.Footer>
+              <Text css={{ color: "#737272", "marginLeft": "auto", "marginRight": "auto" }}>Made with ❤️ by Katsu</Text>
+            </Card.Footer>
+          </Grid.Container>
+        </Card>
+      </Container>
+    </>
   )
 }
