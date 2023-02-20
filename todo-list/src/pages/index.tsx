1d662401c808c7b47ded8a47fb3f6e78796b1d98
NotKatsu <katsudiscord@outlook.com>
2023-02-20 23:37:19 +0000

    Added buttons to confirm and cancel.

--- todo-list/src/pages/index.tsx
+++ todo-list/src/pages/index.tsx
@@ -1,10 +1,15 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import { Card, Grid, Text, Container, Input, Button, Divider, Spacer, Table } from "@nextui-org/react";
+import { BsCheck2 } from 'react-icons/bs'
+import { RxCross2 } from 'react-icons/rx'
+import { Card, Grid, Text, Container, Input, Button, Divider, Spacer, Row } from "@nextui-org/react";
 
 
 export default function Home() {
+  const todos: string[] = [];
+  todos.push("Go shopping and buy eggs, milk and butter.")
+  todos.push("Learn to code a simple todo app in nextjs.")
+  
   return (
     <>
       <Container display="flex" justify="center" alignItems="center" css={{ "minHeight": "100vh" }}>
@@ -14,10 +19,17 @@ export default function Home() {
               <Text h2 css={{ "marginLeft": "auto", "marginRight": "auto" }}>Todo List</Text>
               <Divider/>
               <Spacer y={2} />
-              <Input size="lg"  animated={false} css={{ "width": "80%", "marginLeft": "auto", "marginRight": "auto" }} placeholder="Add a todo..." />
+              <Row>
+                <Input size="lg"  animated={false} css={{ "width": "100%", display: "inline-block" }} placeholder="Add a todo..." />
+                <Button auto css={{ "bg": "#16181A", "color": "#38d976", "marginLeft": "$sm"}}><BsCheck2 size={24}/></Button>
+                <Button auto css={{ "bg": "#16181A", "color": "#d93848", "marginLeft": "$xs"}}><RxCross2 size={24}/></Button>
+              </Row>
             </Card.Body>
           </Grid.Container>
 
+          <Grid.Container>
+
+          </Grid.Container>
 
           <Grid.Container>
             <Card.Footer>
